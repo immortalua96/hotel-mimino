99d3ec27baa2695e28ddbf687158125dff459d62
immortalua96 <romanshvagla@icloud.com>
2023-03-05 23:13:02 +0100

    add slicker

---
+++ src/Js/slick-slider.js
@@ -0,0 +1,14 @@
+// $('.variable-width').slick({
+//   infinite: true,
+//   speed: 300,
+//   slidesToShow: 1,
+//   centerMode: true,
+//   variableWidth: true,
+//   mobileFirst:true
+// });
+$(document).ready(function(){
+	$('.slider').slick({
+		slidesToShow:1,
+		autoplay:false,
+	});
+});
