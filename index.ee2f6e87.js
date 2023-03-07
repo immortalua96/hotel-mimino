f54bd2c8f4715ae1e10f7771b5d37ad1358e6d61
immortalua96 <121064413+immortalua96@users.noreply.github.com>
2023-03-07 17:06:41 +0000

    Deploying to gh-pages from @ immortalua96/hotel-mimino@c7fa6c866e82a4b2aa302549a082240f7f32bc30 🚀

---
+++ index.ee2f6e87.js
@@ -0,0 +1,2 @@
+$(".slider-for").slick({slidesToShow:1,slidesToScroll:1,arrows:!1,fade:!0,asNavFor:".slider-nav"}),$(".slider-nav").slick({slidesToShow:2,slidesToScroll:1,asNavFor:".slider-for",dots:!0,centerMode:!0,focusOnSelect:!0,spaceBetween:20});
+//# sourceMappingURL=index.ee2f6e87.js.map
