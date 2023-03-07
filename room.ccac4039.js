7519a9bb06114d26c53befd9f1dd70472d310dd0
immortalua96 <121064413+immortalua96@users.noreply.github.com>
2023-03-07 20:33:14 +0000

    Deploying to gh-pages from @ immortalua96/hotel-mimino@47ff3914b015d42a2ea545a8ac1f6557beb298b3 🚀

---
+++ room.ccac4039.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".slider__room").slick({slidesToShow:1,speed:800,infinite:!0,autoplay:!0,pauseOnHover:!0,pauseOnFocus:!0,waitForAnimate:!1,centerMode:!0,variableWidth:!0,mobileFirst:!0})}));
+//# sourceMappingURL=room.ccac4039.js.map
