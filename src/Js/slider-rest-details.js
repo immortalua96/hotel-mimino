4fdefe579d9da766009f41c8b7efa0d7edbc24b7
dmytroleskivUA <121440440+dmytroleskivUA@users.noreply.github.com>
2023-03-08 14:57:09 +0000

    new rest-details

--- src/Js/slider-rest-details.js
+++ src/Js/slider-rest-details.js
@@ -1,18 +1,78 @@
-let offset = 0; // зміщення
+// В 4 рядку замінити ".slider__room" на назву свого файлу
 
-const textSLider = document.querySelector('.text_slider');
+$(document).ready(function () {
+  $('.slider-rest-details').slick({
+    // Кількість слайдів
+    slidesToShow: 1,
+    //   Швидкість перемикання
+    speed: 800,
+    // Безкінечність повторення
+    infinite: true,
+    // Автоплей
+    autoplay: true,
+    // Зупинка при ховері та фокусі
+    pauseOnHover: true,
+    pauseOnFocus: true,
+    // Перемикання анімації по швидкості натискання на кнопку
+    waitForAnimate: false,
+    // Центральне позиціювання головного слайду
+    centerMode: true,
+    // Розриви між слайдами в ширину
+    variableWidth: true,
+    // Перший телефон
+    mobileFirst: true,
+    // Адаптивність (в сеттінг пипшемо функцію і значення)
+    // responsive: [
+    //   {
+    //     breakpoint: 1440,
+    //     settings: {},
+    //   },
+    // ],
+  });
+});
 
-document.querySelector('.btn-next').addEventListener('click', function () {
-  offset += 270;
-  if (offset > 540) {
-    offset = 0;
-  }
-  sliderLine.style.left = -offset + 'px';
-});
-document.querySelector('.btn-prev').addEventListener('click', function () {
-  offset -= 270;
-  if (offset < 0) {
-    offset = 540;
-  }
-  sliderLine.style.left = -offset + 'px';
-});
+// $(document).ready(function () {
+//   $('.slider-rest-details').slick({
+//     slidesToShow: 5,
+//     autoplay: false,
+//     speed: 1000,
+//     arrows: true,
+//     responsive: [
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           slidesToShow: 2,
+//         },
+//       },
+//       {
+//         breakpoint: 320,
+//         settings: {
+//           slidesToShow: 2,
+//         },
+//       },
+//     ],
+//   });
+// });
+
+// $('.slider').slick({
+//   arrows: true,
+//   dots: true,
+//   slidesToShow: 3,
+//   autoplay: true,
+//   speed: 1000,
+//   autoplaySpeed: 800,
+//   responsive: [
+//     {
+//       breakpoint: 768,
+//       settings: {
+//         slidesToShow: 2,
+//       },
+//     },
+//     {
+//       breakpoint: 550,
+//       settings: {
+//         slidesToShow: 1,
+//       },
+//     },
+//   ],
+// });
